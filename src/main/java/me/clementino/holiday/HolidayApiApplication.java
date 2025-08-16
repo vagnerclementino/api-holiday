7071606a0b1ac11c7de933e5035190bc83f52862
Vagner Clementino <vagner.clementino@hotmart.com>
2025-08-16 15:52:16 -0300

    feat(dop): remove unused legacy classes and temporary documentation files

--- src/main/java/me/clementino/holiday/HolidayApiApplication.java
+++ src/main/java/me/clementino/holiday/HolidayApiApplication.java
@@ -4,9 +4,11 @@ import me.clementino.holiday.config.HolidayApiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableConfigurationProperties(HolidayApiProperties.class)
+@EnableMongoAuditing
 public class HolidayApiApplication {
 
   public static void main(final String[] args) {
