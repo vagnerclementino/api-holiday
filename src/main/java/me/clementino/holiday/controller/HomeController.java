80df5686846350ecf14b375c64a93b37e7a7d959
Vagner Clementino <vagner.clementino@hotmart.com>
2025-08-17 20:38:19 -0300

    feat(dop): hidden controller

--- src/main/java/me/clementino/holiday/controller/HomeController.java
+++ src/main/java/me/clementino/holiday/controller/HomeController.java
@@ -1,11 +1,13 @@
 package me.clementino.holiday.controller;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /** Home controller providing basic API information. */
 @RestController
+@Hidden
 public class HomeController {
 
   @GetMapping("/")
