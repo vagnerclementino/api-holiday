5a7cd6a5636046b638fd040d9446401ce19a8a44
Vagner Clementino <vagner.clementino@hotmart.com>
2025-09-07 21:10:02 -0300

    refactor: remove configurações não utilizadas
    - Remove HolidayApiProperties.java (não usado no código)
    - Remove @EnableConfigurationProperties do HolidayApiApplication
    - Limpa seção holiday-api dos arquivos de configuração
    - Simplifica application.yml e application-local.yml
    - Reduz complexidade desnecessária seguindo princípio YAGNI

--- src/main/java/me/clementino/holiday/HolidayApiApplication.java
+++ src/main/java/me/clementino/holiday/HolidayApiApplication.java
@@ -1,13 +1,10 @@
 package me.clementino.holiday;
 
-import me.clementino.holiday.config.HolidayApiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
-@EnableConfigurationProperties(HolidayApiProperties.class)
 @EnableMongoAuditing
 public class HolidayApiApplication {
 
