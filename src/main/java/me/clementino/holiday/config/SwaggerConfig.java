5cfe6ebf2b316b4a74d3c0f002aefe21b1429c7c
Vagner Clementino <vagner.clementino@hotmart.com>
2025-09-19 11:30:28 -0300

    style: reformat SwaggerConfig for improved readability

--- src/main/java/me/clementino/holiday/config/SwaggerConfig.java
+++ src/main/java/me/clementino/holiday/config/SwaggerConfig.java
@@ -1,32 +1,36 @@
 package me.clementino.holiday.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-/** Swagger/OpenAPI configuration. */
+/**
+ * Swagger/OpenAPI configuration.
+ */
 @Configuration
 public class SwaggerConfig {
 
-        @Bean
-        public OpenAPI customOpenAPI() {
-                return new OpenAPI()
-                                .info(
-                                                new Info()
-                                                                .title("Holiday API")
-                                                                .description(
-                                                                                "Data-Oriented Programming Holiday API - A comprehensive example demonstrating DOP principles in Java")
-                                                                .version("1.0.0")
-                                                                .contact(
-                                                                                new Contact()
-                                                                                                .name("Vagner Clementino")
-                                                                                                .url("https://github.com/vagnerclementino")
-                                                                                                .email("vagner.clementino@gmail.com"))
-                                                                .license(
-                                                                                new License().name("MIT License").url(
-                                                                                                "https://opensource.org/licenses/MIT")));
-        }
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(
+            new Info()
+                .title("Holiday API")
+                .description("Data-Oriented Programming Holiday API - A comprehensive example demonstrating DOP principles in Java")
+                .version("1.0.0")
+                .contact(
+                    new Contact()
+                        .name("Vagner Clementino")
+                        .url("https://github.com/vagnerclementino")
+                        .email("vagner.clementino@gmail.com"))
+                .license(
+                    new License()
+                        .name("MIT License")
+                        .url("https://opensource.org/licenses/MIT")
+                )
+        );
+  }
 }
